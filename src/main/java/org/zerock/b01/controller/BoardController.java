1420807938e4af6aeda06902aa6ed82093f38921
WonsikJung13 <110441649+WonsikJung13@users.noreply.github.com>
2022-11-17 20:19:24 +0900

    첨부파일 업로드

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -2,6 +2,9 @@ package org.zerock.b01.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,13 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.zerock.b01.dto.BoardDTO;
-import org.zerock.b01.dto.BoardListReplyCountDTO;
-import org.zerock.b01.dto.PageRequestDTO;
-import org.zerock.b01.dto.PageResponseDTO;
+import org.zerock.b01.dto.*;
 import org.zerock.b01.service.BoardService;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
@@ -23,29 +26,20 @@ import javax.validation.Valid;
 @Log4j2
 public class BoardController {
 
-    private final BoardService boardService;
+    @Value("${org.zerock.upload.path}")
+    private String uploadPath;
 
-//    @GetMapping("/list")
-//    public void list(PageRequestDTO pageRequestDTO, Model model) {
-//
-////        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
-//
-//        PageResponseDTO<BoardListReplyCountDTO> responseDTO = boardService.listWithReplyCount(pageRequestDTO);
-//
-//        log.info(responseDTO);
-//
-//        model.addAttribute("responseDTO", responseDTO);
-//
-//    }
+    private final BoardService boardService;
 
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model){
 
         //PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
 
-        PageResponseDTO<BoardListReplyCountDTO> responseDTO =
-                boardService.listWithReplyCount(pageRequestDTO);
-
+//        PageResponseDTO<BoardListReplyCountDTO> responseDTO =
+//                boardService.listWithReplyCount(pageRequestDTO);
+        PageResponseDTO<BoardListAllDTO> responseDTO =
+                boardService.listWithAll(pageRequestDTO);
         log.info(responseDTO);
 
         model.addAttribute("responseDTO", responseDTO);
@@ -117,15 +111,48 @@ public class BoardController {
     }
 
     @PostMapping("/remove")
-    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
 
+        Long bno = boardDTO.getBno();
         log.info("remove post....." + bno);
 
         boardService.remove(bno);
 
+        // 게시물이 데이터베이스 상에서 삭제되었다면 첨부파일 삭제
+        log.info(boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+        if (fileNames != null && fileNames.size() > 0) {
+            removeFiles(fileNames);
+        }
         redirectAttributes.addFlashAttribute("result", "removed");
 
         return "redirect:/board/list";
+    }
+
+    private void removeFiles(List<String> files) {
+
+        for (String fileName:files) {
+
+            Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+            String resourceName = resource.getFilename();
+
+            try {
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+
+                resource.getFile().delete();
+
+                // 섬네일이 존재한다면
+                if (contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+
+                    thumbnailFile.delete();
+                }
+
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
     }
 
 }
